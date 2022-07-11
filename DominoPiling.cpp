fb07e5541dbf6427fee950a1fd52027e97149296
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-11 14:55:34 -0700

    naming updates

--- DominoPiling.cpp
+++ DominoPiling.cpp
@@ -2,36 +2,27 @@
 
 using namespace std;
 
-// "Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, 
-// as long as the contestant earns a positive score..." — an excerpt from contest rules.
+// You are given a rectangular board of M × N squares. 
+// Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. 
+// You are allowed to rotate the pieces. 
+// You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
 
-// A total of n participants took part in the contest (n ≥ k), and you already know their scores. 
-// Calculate how many participants will advance to the next round.
+// 1. Each domino completely covers two squares.
+
+// 2. No two dominoes overlap.
+
+// 3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
+
+// Find the maximum number of dominoes, which can be placed under these restrictions.
 
 // Input
-// The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
-
-// The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), 
-// where ai is the score earned by the participant who got the i-th place. 
-// The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
+// In a single line you are given two integers M and N — board sizes in squares (1 ≤ M ≤ N ≤ 16).
 
 // Output
-// Output the number of participants who advance to the next round.
+// Output one number — the maximal number of dominoes, which can be placed.
 
 int main(){
-    int n, k, advance = 0;
-    cin >> n >> k;
-    int arr[50];
-    for(int i = 0; i < n; ++i){
-        cin >> arr[i];
-    }
-
-    for(int i = 0; i < n; ++i){
-        if(arr[i] > 0 && arr[i] >= arr[k-1]){
-            advance++;
-        }
-    }
-    cout << advance;
+    
 
     return 0;
 }
