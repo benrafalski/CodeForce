a31d90cd51b4d267e06df0ecf6f6e37cb81b8a88
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-07-14 10:47:10 -0700

    903 done

---
+++ 903.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// HQ9+ is a joke programming language which has only four one-character instructions:
+
+// "H" prints "Hello, World!",
+// "Q" prints the source code of the program itself,
+// "9" prints the lyrics of "99 Bottles of Beer" song,
+// "+" increments the value stored in the internal accumulator.
+// Instructions "H" and "Q" are case-sensitive and must be uppercase. The characters of the program which are not instructions are ignored.
+
+// You are given a program written in HQ9+. You have to figure out whether executing this program will produce any output.
+
+// Input
+// The input will consist of a single line p which will give a program in HQ9+. String p will contain between 1 and 100 characters, inclusive. ASCII-code of each character of p will be between 33 (exclamation mark) and 126 (tilde), inclusive.
+
+// Output
+// Output "YES", if executing the program will produce any output, and "NO" otherwise.
+
+int main(){
+    char input[101];
+    cin >> input;
+    for(int i = 0; i < strlen(input); ++i){
+        if(input[i] == 'H' || input[i] == 'Q' || input[i] == '9'){
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+    
+
+    return 0;
+}
