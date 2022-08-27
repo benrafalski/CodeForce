0e6c8a0c7f883769347fd5aedf5e7d5f2b259cce
benrafalski <79751177+benrafalski@users.noreply.github.com>
2022-08-26 17:44:59 -0700

    took some notes

--- tmp2.py
+++ tmp2.py
@@ -7,5 +7,9 @@ from pwn import *
 # print(s.replace(s[0:48], ''))
 
 
-s = '[INFO] This challenge will now perform a bunch of checks.\n[INFO] If you pass these checks, you will receive the flag.\n[TEST] This is a network server. Trying to determine the client process...\n[TEST] Performing tests on the client process!\n[TEST] Checking to make sure that the process is a custom binary that you created by compiling a C program\nTEST] that you wrote. Make sure your C program has a function called \'pwncollege\' in it --- otherwise,\n[TEST] it won\'t pass the checks.\n[INFO] The executable that we are checking is: /home/hacker/a.out.\n[GOOD] You have passed the checks on the client process!\n[INFO] This program will send you 5 mathematical challenges that you will need to compute responses for.\n[TEST] CHALLENGE! Please send the solution for: '
-print(len(s))
+# s = '[GOOD] CORRECT!\n[TEST] CHALLENGE! Please send the solution for: '
+# print(len(s))
+
+
+for i in range(0,81):
+        print('nop')
